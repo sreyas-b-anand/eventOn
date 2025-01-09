852a4d22d3c35e026d84c9309b22248c5baf244f
sreyas <sreyas1045@gmail.com>
2025-01-10 00:46:18 +0530

    added a logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,8 @@
 
 const Navbar = () => {
   return (
-    <div className='w-screen h-20 flex items-center justify-start p-8  bg-card'>
+    <div className='w-screen h-20 flex items-center justify-start p-8  gap-3 bg-card'>
+      <img className="w-[40px] h-[40px] rounded-full"  src="/logo.png" alt="logo" />
       <p className='text-2xl'>EventOn</p>
     </div>
   )
